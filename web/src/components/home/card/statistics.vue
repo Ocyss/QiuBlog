4f98027a151faa3579d0a4fbf264d44f7b924aca
Qiudie <qiudie@88.com>
2022-10-27 18:08:13 +0800

    添加统计信息（后端未实现）

---
+++ web/src/components/home/card/statistics.vue
@@ -0,0 +1,27 @@
+<template>
+  <n-card class="statistics" title="统计信息" size="small">
+    <div v-for="item in data" class="block">
+      <div>{{ item.key }}:</div>
+      <div>{{ item.value }}</div>
+    </div>
+  </n-card>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const data = ref([
+  { key: "文章数目", value: "543" },
+  { key: "已运行时间", value: "45天" },
+  { key: "本站总字数", value: "123.5w" },
+  { key: "当前在线人数", value: "12" },
+  { key: "本站总访问量", value: "34.3w" },
+  { key: "最后更新时间", value: "1天前" },
+]);
+</script>
+
+<style scoped lang="scss">
+.block {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
