a2090a0b82a16abfdbf86ee3732c1a147df748a7
Qiudie <qiudie@88.com>
2022-11-26 08:40:04 +0800

    完善侧栏留言区

--- web/src/components/home/card/comment.vue
+++ web/src/components/home/card/comment.vue
@@ -1,14 +1,20 @@
 <template>
   <n-card class="comment" title="最新留言" size="small">
-    <div class="subitem" v-for="item in 5" :key="item">
-      <n-avatar round size="small">{{ item }}</n-avatar>
+    <div class="subitem" v-for="item in data" :key="item.id">
+      <n-avatar
+        round
+        :size="35"
+        :src="`http://q.qlogo.cn/headimg_dl?dst_uin=${item.qq}&spec=640&img_type=jpg`"
+      />
       <div class="text">
-        <div class="name">{{ item }}(时间)</div>
+        <div class="name">
+          {{ item.name }}
+        </div>
         <div
           class="content"
           :style="{ backgroundColor: randomRgb(130, 250, 0.5) }"
         >
-          评论内容
+          {{ item.content }}
         </div>
       </div>
     </div>
@@ -18,12 +24,29 @@
 <script setup>
 import { ref } from "vue";
 import { randomRgb } from "@/utils";
+import api from "@/api";
+const data = ref([]);
+
+api.message.getMessage({ pagesize: 6, pagenum: 1 }).then((res) => {
+  res.data.map((item) => {
+    if (item.show == true || item.show == undefined) {
+      data.value.push(item);
+    }
+  });
+});
 </script>
 
 <style scoped lang="scss">
+.comment {
+  :deep(.n-card__content) {
+    max-height: 350px;
+    overflow-y: auto;
+  }
+}
+
 .subitem {
   display: flex;
-  line-height: 1;
+  line-height: 1.3;
   .name {
     font-size: 11px;
     line-height: 25px;
